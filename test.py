766c62e43d6d68b08f29bb331c83b6b16f5253f5
panelenelo <panelenelo@gmail.com>
2026-07-23 15:17:32 -0300

    pychall part 3

--- test.py
+++ test.py
@@ -1,35 +1,21 @@
 from icecream import ic
 
 
-class Cache:
-    def __init__(self):
-        self.elements = []
 
-    def insert(self, value):
-        self.elements.append(value)
-        if(len(self.elements) > 4):
-            print("Cache storage limit reached. Popping the cache")
-            self.pop()
-        ic(self.elements)
-
-    def pop(self):
-        self.elements.pop(0)
-
-    
-# Implement the same thing now using collections,
-# in this case, using deque
 
 
 
 def main():
-    cache = Cache()
-    cache.insert(4)
-    cache.insert(3)
-    cache.insert(2)
-    cache.insert(6)
-    cache.insert(412)
-    cache.insert(44)
-    cache.insert(47)
+    data = []
+    with open('text2.txt', 'r') as f:
+        data = f.read()
+
+    for i in data:
+        if (64 < ord(i) < 91 or 96 < ord(i) < 123):
+            ic(i)
+
+    
+    
 
     
 
